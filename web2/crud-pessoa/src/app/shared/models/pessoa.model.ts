84f00dadf164e579938b49fef73cdf433f4eb6ea
Igor Juan Cordeiro da Costa <igorjuancc@gmail.com>
2025-11-15 20:33:02 -0300

    web2 - formularios - implementacao de radio buttom para carteira de motorista

--- web2/crud-pessoa/src/app/shared/models/pessoa.model.ts
+++ web2/crud-pessoa/src/app/shared/models/pessoa.model.ts
@@ -2,7 +2,8 @@ export class Pessoa {
     constructor(
         public id?: number,
         public nome?: string,
-        public idade?: number,
-        public dataDeNascimento?: string
+        public idade: number = 0,
+        public dataDeNascimento?: string,
+        public motorista?: string
     ) { }
 }

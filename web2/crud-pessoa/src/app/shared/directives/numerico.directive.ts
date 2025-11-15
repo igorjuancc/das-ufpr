e3d71d3143754c8c4c8763d0b1162e0c01c87237
Igor Juan Cordeiro da Costa <igorjuancc@gmail.com>
2025-11-15 12:12:03 -0300

    web2 - diretiva numerico - adicionado controlador de acesso para sincronizar tela com modelo

--- web2/crud-pessoa/src/app/shared/directives/numerico.directive.ts
+++ web2/crud-pessoa/src/app/shared/directives/numerico.directive.ts
@@ -1,12 +1,31 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
   selector: '[numerico]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumericoDirective,
+    multi: true
+  }],
   standalone: false
 })
-export class NumericoDirective {
+export class NumericoDirective implements ControlValueAccessor {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
+
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  writeValue(value: any): void {
+    this.el.nativeElement.value = value;
+  }
 
   @HostListener('keyup', ['$event'])
   onKeyUp($event: any) {
@@ -14,5 +33,8 @@ export class NumericoDirective {
     // expressão regular: remove tudo que não é número
     valor = valor.replace(/[\D]/g, '');
     $event.target.value = valor;
+
+    // atualiza o model
+    this.onChange(valor);
   }
 }

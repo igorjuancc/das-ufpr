eaaa427d7df33bb589b2daf77c3d35b496d8a0a8
Igor Juan Cordeiro da Costa <igorjuancc@gmail.com>
2025-11-17 17:21:00 -0300

    web2 - web service - implementacao de inserir-editar usuario

--- web2/crud-pessoa/src/app/usuario/inserir-editar-usuario/inserir-editar-usuario.component.ts
+++ web2/crud-pessoa/src/app/usuario/inserir-editar-usuario/inserir-editar-usuario.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Usuario } from '../../shared';
+import { UsuarioService } from '../services';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-inserir-editar-usuario',
@@ -6,6 +10,56 @@ import { Component } from '@angular/core';
   templateUrl: './inserir-editar-usuario.component.html',
   styleUrl: './inserir-editar-usuario.component.css'
 })
-export class InserirEditarUsuarioComponent {
 
+export class InserirEditarUsuarioComponent implements OnInit {
+  @ViewChild('formUsuario') formUsuario!: NgForm;
+  novoUsuario: boolean = true;
+  usuario: Usuario = new Usuario();
+  id!: string;
+  loading!: boolean;
+  senhaAntiga: string = "";
+  constructor(
+    private usuarioService: UsuarioService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.usuario = new Usuario();
+    this.loading = false;
+    this.id = this.route.snapshot.params['id'];
+    this.novoUsuario = !this.id;
+    if (!this.novoUsuario) {
+      this.usuarioService.buscarPorId(+this.id).subscribe(
+        usuario => {
+          this.usuario = usuario;
+          this.senhaAntiga = usuario.senha ? usuario.senha : "";
+          this.usuario.senha = "";
+        }
+      );
+    }
+  }
+  
+  salvar(): void {
+    this.loading = true;
+    if (this.formUsuario.form.valid) {
+      if (this.novoUsuario) {
+        this.usuarioService.inserir(this.usuario).subscribe(
+          usuario => {
+            this.loading = false;
+            this.router.navigate(["/usuarios"]);
+          });
+      }
+      else {
+        if (this.usuario.senha === "") {
+          this.usuario.senha = this.senhaAntiga;
+        }
+        this.usuarioService.alterar(this.usuario).subscribe(
+          usuario => {
+            this.loading = false;
+            this.router.navigate(["/usuarios"]);
+          });
+      }
+    }
+    this.loading = false;
+  }
 }

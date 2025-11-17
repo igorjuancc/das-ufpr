a4556c32b0b58432106847c2f5d64885a9d88603
Igor Juan Cordeiro da Costa <igorjuancc@gmail.com>
2025-11-17 14:42:20 -0300

    web2 - login - permissoes de menus e logout

--- web2/crud-pessoa/src/app/app.component.ts
+++ web2/crud-pessoa/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './auth/services';
+import { Usuario } from './shared';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cadastro de Pessoas';
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService) { }
+
+  get usuarioLogado(): Usuario | null {
+    return this.loginService.usuarioLogado;
+  }
+
+  logout() {
+    this.loginService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  temPermissao(...perfis: string[]): boolean {
+    let usu = this.usuarioLogado;
+    if (usu != null && perfis.length > 0) {
+      for (let p of perfis) {
+        if (usu?.perfil?.indexOf(p) != -1) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }

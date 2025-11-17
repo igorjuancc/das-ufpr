1484bc47341b02c4449c57f134971f866596bc41
Igor Juan Cordeiro da Costa <igorjuancc@gmail.com>
2025-11-17 17:17:25 -0300

    web2 - web service - implementacao de listar usuarios

--- web2/crud-pessoa/src/app/usuario/listar-usuario/listar-usuario.component.ts
+++ web2/crud-pessoa/src/app/usuario/listar-usuario/listar-usuario.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalUsuarioComponent } from '../modal-usuario';
+import { Usuario } from '../../shared';
+import { UsuarioService } from '../services';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-listar-usuario',
@@ -6,6 +10,38 @@ import { Component } from '@angular/core';
   templateUrl: './listar-usuario.component.html',
   styleUrl: './listar-usuario.component.css'
 })
-export class ListarUsuarioComponent {
 
+export class ListarUsuarioComponent implements OnInit {
+  usuarios: Usuario[] = [];
+  constructor(private usuarioService: UsuarioService,
+    private modalService: NgbModal) { }
+  ngOnInit(): void {
+    this.listarTodos();
+  }
+  listarTodos(): Usuario[] {
+    this.usuarioService.listarTodos().subscribe({
+      next: (data: Usuario[]) => {
+        if (data == null) {
+          this.usuarios = [];
+        }
+        else {
+          this.usuarios = data;
+        }
+      }
+    });
+    return this.usuarios;
+  }
+  remover($event: any, usuario: Usuario): void {
+    $event.preventDefault();
+    if (confirm(`Deseja realmente remover o usuário ${usuario.nome}?`)) {
+      this.usuarioService.remover(usuario.id!).
+        subscribe({
+          complete: () => { this.listarTodos(); }
+        });
+    }
+  }
+  abrirModal(usuario: Usuario) {
+    const modalRef = this.modalService.open(ModalUsuarioComponent);
+    modalRef.componentInstance.usuario = usuario;
+  }
 }

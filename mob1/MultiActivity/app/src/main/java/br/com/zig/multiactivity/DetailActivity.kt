b98cd816202f03962d9282a182ad3d80dc266fae
Igor Juan Cordeiro Da Costa <igorjuancc@gmail.com>
2025-12-30 18:35:28 -0300

    mob1 - multi activity app - commit inicial

---
+++ mob1/MultiActivity/app/src/main/java/br/com/zig/multiactivity/DetailActivity.kt
@@ -0,0 +1,28 @@
+package br.com.zig.multiactivity
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val closeButtom = findViewById<Button>(R.id.closeButton)
+        closeButtom.setOnClickListener() {
+            finish()
+        }
+    }
+}
